a3e43602752c2304fdadfb3f0724f7bc67d72410
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-23 12:14:31 +0500

    -Database Schema init (#5)

--- src/main/kotlin/com/realtor/plugins/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/realtor/plugins/repository/DatabaseFactory.kt
@@ -1,9 +1,20 @@
 package com.realtor.plugins.repository
 
+import com.realtor.plugins.data.CategoriesTable
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
+
+    fun init(){
+        Database.connect(hikari())
+        transaction {
+            SchemaUtils.create(CategoriesTable)
+        }
+    }
 
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
