3d24b5b3d87b30e0f32dc562e683fe0003702838
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-23 15:44:03 +0500

    -Categroy Route Impl (#14)

--- src/main/kotlin/com/realtor/plugins/routes/Routes.kt
+++ src/main/kotlin/com/realtor/plugins/routes/Routes.kt
@@ -1,7 +1,14 @@
 package com.realtor.plugins.routes
 
 import com.realtor.API_VERSION
+import com.realtor.plugins.repository.CategoriesRepository
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
 import io.ktor.server.locations.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 const val CATEGORY_CREATE = "$API_VERSION/category"
 const val GET_CATEGORY_BY_ID = "$API_VERSION/category/{id}"
@@ -13,3 +20,31 @@ class CategoryCreateRoute
 @OptIn(KtorExperimentalLocationsAPI::class)
 @Location(GET_CATEGORY_BY_ID)
 class GetCategoryById
+
+fun Route.category(
+    db: CategoriesRepository
+) {
+    post(CATEGORY_CREATE) {
+        val parameters = call.receive<Parameters>()
+        val name = parameters["name"] ?: return@post call.respondText(
+            text = "MISSING FIELD",
+            status = HttpStatusCode.Unauthorized
+        )
+        val priority = parameters["priority"] ?: return@post call.respondText(
+            text = "MISSING FIELD",
+            status = HttpStatusCode.Unauthorized
+        )
+
+        try {
+            val category =  db.insert(name, priority.toInt())
+            category?.id?.let {
+                call.respond(status = HttpStatusCode.OK, category)
+
+            }
+
+
+        }catch (e: Throwable){
+            call.respondText("${e.message}")
+        }
+    }
+}
