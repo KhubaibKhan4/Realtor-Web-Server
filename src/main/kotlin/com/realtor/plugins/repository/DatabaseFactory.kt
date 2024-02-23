64752083717858a0a35d2c0466acd652073788c0
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-23 10:45:55 +0500

    -Hikrai DB Setup (#5)

---
+++ src/main/kotlin/com/realtor/plugins/repository/DatabaseFactory.kt
@@ -0,0 +1,18 @@
+package com.realtor.plugins.repository
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+
+object DatabaseFactory {
+
+    private fun hikari(): HikariDataSource {
+        val config = HikariConfig()
+        config.driverClassName = System.getenv("JDBC_DRIVER")
+        config.jdbcUrl = System.getenv("JDBC_DATABASE_URL")
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+        return HikariDataSource(config)
+    }
+}
