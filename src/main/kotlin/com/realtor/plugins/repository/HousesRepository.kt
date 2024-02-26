bb6370502c6351a7d3039a92987bf6424db55e3e
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-26 15:56:47 +0500

    -Delete Houses By Id Impl (#37)

--- src/main/kotlin/com/realtor/plugins/repository/HousesRepository.kt
+++ src/main/kotlin/com/realtor/plugins/repository/HousesRepository.kt
@@ -3,10 +3,8 @@ package com.realtor.plugins.repository
 import com.realtor.plugins.dao.HousesDao
 import com.realtor.plugins.data.model.Houses
 import com.realtor.plugins.data.table.HousesTable
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.statements.InsertStatement
 
 class HousesRepository : HousesDao {
@@ -44,7 +42,11 @@ class HousesRepository : HousesDao {
     }
 
     override suspend fun deleteHouseById(id: Int): Int? {
-        TODO("Not yet implemented")
+        return DatabaseFactory.dbQuery {
+            HousesTable.deleteWhere {
+                HousesTable.id.eq(id)
+            }
+        }
     }
 
     override suspend fun updateHouseById(title: String, price: String, type: String, size: String, rooms: Int): Int? {
