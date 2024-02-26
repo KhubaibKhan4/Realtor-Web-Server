f47872eeffc5ca80f0f136b016a915e6d8a977e4
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-26 15:33:57 +0500

    -Houses Dao Init (#25)

---
+++ src/main/kotlin/com/realtor/plugins/dao/HousesDao.kt
@@ -0,0 +1,13 @@
+package com.realtor.plugins.dao
+
+import com.realtor.plugins.data.model.Houses
+
+interface HousesDao {
+    suspend fun insert(
+        title: String,
+        price: String,
+        type: String,
+        size: String,
+        rooms: Int
+    ): Houses?
+}
