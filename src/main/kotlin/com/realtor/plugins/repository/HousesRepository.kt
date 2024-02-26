4ea32fcc80df4b1ba6b47e0dcd602fc7c8e0953b
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-26 15:49:51 +0500

    -Result Row DB Impl (#33)

--- src/main/kotlin/com/realtor/plugins/repository/HousesRepository.kt
+++ src/main/kotlin/com/realtor/plugins/repository/HousesRepository.kt
@@ -3,6 +3,7 @@ package com.realtor.plugins.repository
 import com.realtor.plugins.dao.HousesDao
 import com.realtor.plugins.data.model.Houses
 import com.realtor.plugins.data.table.HousesTable
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.statements.InsertStatement
 
@@ -34,5 +35,20 @@ class HousesRepository : HousesDao {
 
     override suspend fun updateHouseById(title: String, price: String, type: String, size: String, rooms: Int): Int? {
         TODO("Not yet implemented")
+    }
+
+    fun rowToResult(row: ResultRow): Houses? {
+        if (row == null) {
+            return null
+        } else {
+            return Houses(
+                id = row[HousesTable.id],
+                title = row[HousesTable.title],
+                price = row[HousesTable.price],
+                type = row[HousesTable.type],
+                size = row[HousesTable.size],
+                rooms = row[HousesTable.rooms]
+            )
+        }
     }
 }
