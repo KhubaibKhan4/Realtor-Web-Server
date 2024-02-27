9e87e45c82f3d052088a3589117f0a3a71af5e26
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-27 16:23:50 +0500

    -Contact Table Impl (#52)

--- src/main/kotlin/com/realtor/plugins/data/table/ContactTable.kt
+++ src/main/kotlin/com/realtor/plugins/data/table/ContactTable.kt
@@ -1,6 +1,12 @@
 package com.realtor.plugins.data.table
 
+import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
 object ContactTable : Table() {
+    val id: Column<Int> = integer("id").autoIncrement()
+    val name: Column<String> = varchar("name", length = 500)
+    val email: Column<String> = varchar("email", length = 500)
+    val message: Column<String> = varchar("message", length = 2000)
+    override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
