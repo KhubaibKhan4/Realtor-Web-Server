f3525ea3e73c644de56e536a1632af7e53a45172
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-28 16:12:32 +0500

    -Images Model Impl (#78)

---
+++ src/main/kotlin/com/realtor/plugins/data/model/Images.kt
@@ -0,0 +1,9 @@
+package com.realtor.plugins.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Images(
+    val imageUrl: String,
+    val contentDescription: String
+)
