4d43ec3631d0df97025ed08722d4db85a1d4c6b0
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-28 16:37:42 +0500

    -Id Impl (#92)

--- src/main/kotlin/com/realtor/plugins/data/table/ImagesTable.kt
+++ src/main/kotlin/com/realtor/plugins/data/table/ImagesTable.kt
@@ -4,6 +4,8 @@ import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
 object ImagesTable : Table() {
+    val id: Column<Int> = integer("id").autoIncrement()
     val imageUrl: Column<String> = varchar("imageUrl", length = 2000)
     val description: Column<String> = varchar("description", length = 1000)
+    override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
