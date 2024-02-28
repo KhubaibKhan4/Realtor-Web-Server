5b6e9430e9de6638199eb7e5d3f966f0feeabc03
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-28 16:18:10 +0500

    -Images Table Impl (#81)

--- src/main/kotlin/com/realtor/plugins/data/table/ImagesTable.kt
+++ src/main/kotlin/com/realtor/plugins/data/table/ImagesTable.kt
@@ -1,6 +1,9 @@
 package com.realtor.plugins.data.table
 
+import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object ImagesTable: Table() {
+object ImagesTable : Table() {
+    val imageUrl: Column<String> = varchar("imageUrl", length = 2000)
+    val description: Column<String> = varchar("description", length = 1000)
 }
