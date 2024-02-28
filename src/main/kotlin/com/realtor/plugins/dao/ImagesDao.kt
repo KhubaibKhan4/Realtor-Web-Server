bc6e69d8172e26127e41714600611367735ad25f
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-28 16:26:25 +0500

    -Get All Images List Impl (#86)

--- src/main/kotlin/com/realtor/plugins/dao/ImagesDao.kt
+++ src/main/kotlin/com/realtor/plugins/dao/ImagesDao.kt
@@ -7,6 +7,7 @@ interface ImagesDao {
         imageUrl: String,
         description: String
     ): Images?
+    suspend fun getAllImages(): List<Images>?
     suspend fun getImagesById(id: Int): Images?
     suspend fun deleteImagesById(id: Int): Int?
     suspend fun updateImagesById(id: Int): Int?
