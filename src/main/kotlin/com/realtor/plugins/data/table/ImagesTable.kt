a1240d5463c678b42866f4950ef157d83099385e
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-29 15:35:38 +0500

    -Category Id Reference Impl in ImagesTable (#113)

--- src/main/kotlin/com/realtor/plugins/data/table/ImagesTable.kt
+++ src/main/kotlin/com/realtor/plugins/data/table/ImagesTable.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.sql.Table
 
 object ImagesTable : Table() {
     val id: Column<Int> = integer("id").autoIncrement()
+    val categoryId: Column<Int> = integer("category_id").references(CategoriesTable.id)
     val imageUrl: Column<String> = varchar("imageUrl", length = 10000)
     val description: Column<String> = varchar("description", length = 1000)
     override val primaryKey: PrimaryKey = PrimaryKey(id)
