01f265797a393d1415aabc528f247d47e3e250fa
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-29 15:43:23 +0500

    -Get Categories With Houses List Impl (#115)

--- src/main/kotlin/com/realtor/plugins/repository/CategoriesRepository.kt
+++ src/main/kotlin/com/realtor/plugins/repository/CategoriesRepository.kt
@@ -2,7 +2,10 @@ package com.realtor.plugins.repository
 
 import com.realtor.plugins.dao.CategoriesDao
 import com.realtor.plugins.data.model.Categories
+import com.realtor.plugins.data.model.CategoryWithHouses
+import com.realtor.plugins.data.model.Houses
 import com.realtor.plugins.data.table.CategoriesTable
+import com.realtor.plugins.data.table.HousesTable
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.statements.InsertStatement
@@ -49,6 +52,28 @@ class CategoriesRepository : CategoriesDao {
             }
         }
 
+    override suspend fun getCategoryWithHouses(): List<CategoryWithHouses> {
+        return DatabaseFactory.dbQuery {
+            CategoriesTable.selectAll().map {categoryRow ->
+                val category = rowToCategory(categoryRow)
+                val houses = HousesTable.select { HousesTable.categoryId eq category?.id!! }.map {houseRow ->
+                    rowToHouse(houseRow)
+                }
+                CategoryWithHouses(category!!,houses)
+            }
+        }
+    }
+    private fun rowToHouse(row: ResultRow): Houses {
+        return Houses(
+            id = row[HousesTable.id],
+            categoryId = row[HousesTable.categoryId],
+            title = row[HousesTable.title],
+            price = row[HousesTable.price],
+            type = row[HousesTable.type],
+            size = row[HousesTable.size],
+            rooms = row[HousesTable.rooms]
+        )
+    }
     private fun rowToCategory(row: ResultRow): Categories? {
         if (row == null) {
             return null
