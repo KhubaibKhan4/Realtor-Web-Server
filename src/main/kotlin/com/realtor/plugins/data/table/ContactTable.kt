1b2f884ff268ca70d1cc7ec0ca67480b3fff8af5
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-01 11:03:09 +0500

    -Contact Table Enhancement (#134)

--- src/main/kotlin/com/realtor/plugins/data/table/ContactTable.kt
+++ src/main/kotlin/com/realtor/plugins/data/table/ContactTable.kt
@@ -3,7 +3,7 @@ package com.realtor.plugins.data.table
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object ContactTable : Table() {
+object ContactTable : Table(name = "Contact") {
     val id: Column<Int> = integer("id").autoIncrement()
     val name: Column<String> = varchar("name", length = 500)
     val email: Column<String> = varchar("email", length = 500)
