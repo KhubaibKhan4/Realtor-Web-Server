2fe6ee1017b7c61ac2fe06819b2b03eb12ee51f7
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-01 11:04:54 +0500

    -Images Table Enhancement (#134)

--- src/main/kotlin/com/realtor/plugins/data/table/ImagesTable.kt
+++ src/main/kotlin/com/realtor/plugins/data/table/ImagesTable.kt
@@ -3,7 +3,7 @@ package com.realtor.plugins.data.table
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object ImagesTable : Table() {
+object ImagesTable : Table(name = "Images") {
     val id: Column<Int> = integer("id").autoIncrement()
     val imageUrl: Column<String> = varchar("imageUrl", length = 10000)
     val description: Column<String> = varchar("description", length = 1000)
