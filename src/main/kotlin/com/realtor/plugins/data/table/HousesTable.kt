75ae73cd77a52985938ba20b35c880f5345a4582
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-01 17:41:53 +0500

    -Images Table Foriegn Key with HousesTable (#157)

--- src/main/kotlin/com/realtor/plugins/data/table/HousesTable.kt
+++ src/main/kotlin/com/realtor/plugins/data/table/HousesTable.kt
@@ -9,7 +9,7 @@ object HousesTable : Table(name = "Houses") {
     val categoryId: Column<Long> =
         long("categoryId").references(ref = CategoriesTable.id, onDelete = ReferenceOption.CASCADE)
     val categoryTitle: Column<String> =
-        varchar("categoryTitle", length = 300) //.references(ref = CategoriesTable.name, ReferenceOption.CASCADE)
+        varchar("categoryTitle", length = 300)
     val title: Column<String> = varchar("title", length = 1500)
     val price: Column<String> = varchar("price", length = 1500)
     val type: Column<String> = varchar("type", length = 1500)
