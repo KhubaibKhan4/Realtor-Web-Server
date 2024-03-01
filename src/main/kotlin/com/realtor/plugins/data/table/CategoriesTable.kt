c133de86a74c0afad123936bdf9fb1ec45c8c695
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-01 10:59:41 +0500

    -Categories Table Enhancement (#133)

--- src/main/kotlin/com/realtor/plugins/data/table/CategoriesTable.kt
+++ src/main/kotlin/com/realtor/plugins/data/table/CategoriesTable.kt
@@ -3,7 +3,7 @@ package com.realtor.plugins.data.table
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object CategoriesTable : Table() {
+object CategoriesTable : Table(name = "Categories") {
 
     val id: Column<Int> = integer("id").autoIncrement()
     val name: Column<String> = varchar("name", length = 51)
